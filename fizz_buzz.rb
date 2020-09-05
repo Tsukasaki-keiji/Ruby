2d7fdfda448c0378291a8a9eb9a6a4d932944e4e
EC2 Default User <ec2-user@ip-172-31-72-178.ec2.internal>
2020-09-05 07:04:59 +0000

    first commit 条件式記述

---
+++ fizz_buzz.rb
@@ -0,0 +1,12 @@
+
+def fizz_buzz(number)
+  if number 15 == 0
+    puts "FizzBuzz"
+  elsif number 3 == 0
+    puts "Fizz"
+  elsif number 5 == 0
+    puts "Buzz"
+  else
+    number.to_s
+  end
+end
